983ac5f0873867234d3cd7210a391ceb9b8d858b
samoudud <moududpro@gmail.com>
2021-11-13 04:35:33 +0600

    admin

--- index.js
+++ index.js
@@ -91,7 +91,20 @@ async function run() {
             const docs = { $set: user };
             const result = await usersCollection.updateOne(filter, docs, options);
             res.json(result);
+        });
+
+        // get admin api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
+
 
         /* 
 
